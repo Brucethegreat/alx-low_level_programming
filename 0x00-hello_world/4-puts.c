bf177ec663f8d373cd74155aab4f97aba89ff7d2
Brucethegreat <sitibruce@gmail.com>
2022-04-07 14:54:15 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints a phrase
+ *
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+
+}
